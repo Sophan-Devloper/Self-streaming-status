cbe615e97d963a94be004bca19b5516148c66cc6
Un Sophan <82254060+Sophan-Devloper@users.noreply.github.com>
2021-07-29 14:54:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client } = require('discord.js-selfbot-v11')
+const BOT = new Client();
+const { TOKEN } = require('./config.json');
+BOT.on('ready', () => { 
+    BOT.user.setActivity("with depression", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/monstercat"
+      });
+   console.log(`${BOT.user.username} Successfully Logged in!`)
+})
+BOT.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+  });
+
+BOT.login(TOKEN);
