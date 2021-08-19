fa6feb26d5335ab1a6575f1a415ce03d7e77628f
Un Sophan <82254060+Sophan-Devloper@users.noreply.github.com>
2021-08-19 11:26:52 +0700

    change to config.json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client } = require('discord.js-selfbot-v11')
 const BOT = new Client();
-const { TOKEN } = require('./config.json');
+const { TOKEN, STATUS , TwitchURL } = require('./config.json');
 BOT.on('ready', () => { 
-    BOT.user.setActivity("with depression", {
+    BOT.user.setActivity(`${STATUS}`, {
         type: "STREAMING",
-        url: "https://www.twitch.tv/monstercat"
+        url: TwitchURL
       });
    console.log(`${BOT.user.username} Successfully Logged in!`)
 })
