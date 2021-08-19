3c53e715556c263bfae320eb209541971cc90cb3
Un Sophan <82254060+Sophan-Devloper@users.noreply.github.com>
2021-08-19 11:27:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { TOKEN, STATUS , TwitchURL } = require('./config.json');
 BOT.on('ready', () => { 
     BOT.user.setActivity(`${STATUS}`, {
         type: "STREAMING",
-        url: TwitchURL
+        url: `${TwitchURL}`
       });
    console.log(`${BOT.user.username} Successfully Logged in!`)
 })
